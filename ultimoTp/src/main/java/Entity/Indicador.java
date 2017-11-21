ad35b53db402d8830398b2febeb89a648fef8288
DESKTOP-JRVK2JV\Daniela <dani.montanaro21@gmail.com>
2017-11-21 15:11:15 -0300

    cambios periodo

--- ultimoTp/src/main/java/Entity/Indicador.java
+++ ultimoTp/src/main/java/Entity/Indicador.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Query;
 import javax.persistence.Table;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -139,6 +140,28 @@ public class Indicador implements Entidad {
 				formula=(String) em.createNativeQuery("Select formula from indicador where nombre =' "+nombreIndicador+" ' ").getSingleResult();
 	 	return formula;
 	}
+	public boolean perteneceAIndicador(String indicador)
+	{
+		ArrayList<String> ctas1 = null;
+		
+		ctas1 = getNombresIndicadoresSQL();
+		//ArrayList<String> ctas = this.getNombresCuentasPorPeriodo(desde,hasta, empresa);
+		//me devulve las cuentas que tiene esa empresa en ese periodo
+		return ctas1.contains(indicador); // se fija si la cuenta que quiero , esta en esa lista de cuentas por empresa
+	}
+	
+	public ArrayList<String> getNombresIndicadoresSQL()
+	{
+	ArrayList<String> indicador1 = null;
+	EntityManager indicador = EntityManagerHelper.entityManager();
+	Query q = indicador.createNativeQuery("select formula from indicador");
+	@SuppressWarnings("unchecked")
+	ArrayList<String> authors = (ArrayList<String>) q.getResultList();
+	//System.out.println((ArrayList<String>) ctas.createNativeQuery("Select C.nombre from cuenta C join empresa_cuenta EC on (C.id = EC.cuenta_id) join empresa E on (E.id = EC.empresa_id) join periodo P on (P.cuenta_empresa = EC.id ) where E.nombre='"+empresa+"' and P.desde='"+desde+"' and P.hasta='"+hasta+"'").getSingleResult());
+	return authors;
+	}
+	
+	
 	//cuando usas esta funcion , poner a contadorDeLoop en 0 
 	public String sacarIndicadores (String formula,String desde,String hasta,String empresa,int contadorDeLoop) {
 		Cuenta cuenta= new Cuenta();
@@ -149,6 +172,7 @@ public class Indicador implements Entidad {
 	    for(int i=contadorDeLoop;i<valores.size();i++){
 	    	String cuentaInd = valores.get(i);
 	    	if (!cuenta.perteneceALasCuentas(cuentaInd, desde, hasta, empresa)){
+	    		//if(perteneceAIndicador(cuentaInd)){
 	            String formulaNueva = getFormulaDeIndicador(cuentaInd);
 	            String[] parts = formula.split(cuentaInd);
 	            StringBuilder formulaFinal = new StringBuilder();
@@ -165,7 +189,12 @@ public class Indicador implements Entidad {
 	            i--;
 	            //formulaDevuelta = sacarIndicadores(formulaFinal.toString(),desde,hasta,empresa,i);
 	            //break; 
-	        }
+	    		/*}
+	    		else 
+	    		{
+	    			System.out.println("La cuenta no pertenece a la empresa en el periodo"); 
+	    		}*/
+	    	}
 	    }
 	    return formulaDevuelta;
 	}
@@ -184,7 +213,6 @@ public class Indicador implements Entidad {
 		Cuenta cuenta = new Cuenta();
 		Indicador indicadores =  new Indicador();
 	
-		
 		formulaFinal = indicadores.sacarIndicadores(formula,desde,hasta,empresa,0);
 		valoresDeCuentas = cuenta.getValorFormula(formulaFinal,desde,hasta,empresa);
 		valoresFormulaFinal = cuenta.cuentasDeLaFormula(formula);
